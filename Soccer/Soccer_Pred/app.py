17dbd8317b7c1ccc0070a538447a38348f1b24ba
Powei Shadrack <poweishadrack@gmail.com>
2026-06-29 17:28:28 +0100

    Add soccer prediction UI

--- Soccer/Soccer_Pred/app.py
+++ Soccer/Soccer_Pred/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("Hello, Streamlit 👋")
-st.write("This is my first Streamlit app!")
+st.title("Hello, welcome to the home of soccer prediction for fun.")
+st.write("This is my first soccer prediction model. Hopefully, we will add other sports predictions soon. Enjoy, and happy predicting!")
+API_BASE_URL = "https://https-github-com-sunspot-tech-ml-journey.onrender.com"
